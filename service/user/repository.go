4e6b49cfb718c14b8a9580ea9e5057c30dc64e96
Avelia Putri Elka Pramana <aveliaputri02@gmail.com>
2024-10-05 19:29:05 +0700

    update: create user

--- service/user/repository.go
+++ service/user/repository.go
@@ -3,6 +3,7 @@ package user
 import (
 	"database/sql"
 	"fmt"
+	"log"
 
 	"github.com/aveliap/transaction-go/types"
 )
@@ -34,6 +35,39 @@ func (repo *Repo) GetUserByEmail(email string) (*types.User, error) {
 	return u, nil
 }
 
+
+func (repo *Repo) GetUserByID (id uint) (*types.User, error)  {
+	rows, err := repo.db.Query("SELECT * FROM users WHERE id=?", id)
+
+	if err != nil {
+		return nil,err
+	}
+	u := new(types.User)
+	for rows.Next(){
+		u, err = scanRowIntoUser(rows)
+		if err != nil {
+			return nil,err
+		}
+	}
+	if u.ID == 0 {
+		return nil, fmt.Errorf("user not found")
+	}
+	return u, nil
+}
+
+func (repo *Repo) CreateUser (user types.User) error  {
+	fmt.Printf("firstName: %s, lastName: %s, email: %s, password: %s\n", user.FirstName, user.LastName, user.Email, user.Password)
+	log.Printf("Executing query: INSERT INTO users (\"firstName\", \"lastName\", \"email\", \"password\") VALUES (%s, %s, %s, %s)",
+    user.FirstName, user.LastName, user.Email, user.Password)
+
+	_, err := repo.db.Exec("INSERT INTO users (\"firstName\", \"lastName\", \"email\", \"password\") VALUES ($1, $2, $3, $4)",
+				user.FirstName,user.LastName,user.Email,user.Password)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func scanRowIntoUser(row *sql.Rows)(*types.User, error)  {
 	user := new(types.User)
 
@@ -50,12 +84,4 @@ func scanRowIntoUser(row *sql.Rows)(*types.User, error)  {
 	}
 
 	return user, nil
-}
-
-func (repo *Repo) GetUserByID (id uint) (*types.User, error)  {
-	return nil,nil	
-}
-
-func (repo *Repo) CreateUser (user types.User) error  {
-	return nil
 }
